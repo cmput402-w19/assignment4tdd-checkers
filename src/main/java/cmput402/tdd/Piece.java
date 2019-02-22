3955e9492e5c066a58ecce063dbc577aa52772b0
hiufungk <hiufungk@ualberta.ca>
2019-02-22 16:18:57 -0700

    GREEN added implmentation for Piece class constructor

--- src/main/java/cmput402/tdd/Piece.java
+++ src/main/java/cmput402/tdd/Piece.java
@@ -1,7 +1,28 @@
 package cmput402.tdd;
 
 public abstract class Piece {
-	public Piece(String color, int xPosition, int yPosition) {
+	
+	private int xPosition;
+	private int yPosition;
+	private String color;
+	
+	public Piece(String color, int xPosition, int yPosition) throws Exception {
+		if(color == "R" || color == "B") {
+			this.color = color;
+		} else {
+			throw new Exception("invalid color");
+		}
 		
+		if(xPosition < 0 || xPosition >= 8) {
+			throw new Exception("invalid xPosition");
+		} else {
+			this.xPosition = xPosition;
+		}
+		
+		if(yPosition < 0 || yPosition >= 8) {
+			throw new Exception("invalid yPosition");
+		} else {
+			this.yPosition = yPosition;
+		}
     }
 }
