98c178e626fe4653d48cf4208e99cbc86ccbdd3e
hiufungk <hiufungk@ualberta.ca>
2019-02-21 20:32:48 -0700

    RED added skeleton for Piece class

---
+++ src/main/java/cmput402/tdd/Piece.java
@@ -0,0 +1,28 @@
+package cmput402.tdd;
+
+public abstract class Piece {
+	
+	public Piece(String color, int xPosition, int yPosition) throws Exception{
+		
+    }
+	
+	public String getColor() {
+		return null;
+	}
+	
+	public int getXPosition() {
+		return (Integer) null;
+	}
+	
+	public int getYPosition() {
+		return (Integer) null;
+	}
+	
+	public void setPosition(int x, int y) throws Exception {
+		
+	}
+	
+	protected Boolean isEnemyPiece(Piece targetPiece) {
+		return null;
+	}
+}
