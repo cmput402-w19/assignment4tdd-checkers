1fc04765ea59eba4ff397a93ee18f912761d3adc
htruong1 <htruong1@ualberta.ca>
2019-02-22 21:55:41 -0700

    RED Test for board state instantiation fails

--- src/test/java/cmput402/tdd/BoardTest.java
+++ src/test/java/cmput402/tdd/BoardTest.java
@@ -35,6 +35,41 @@ public class BoardTest extends TestCase {
 	    assertNull(checkersBoard.getBoardPiece(-1, 2));
     }
 
+    /**
+     * Unit test to test the proper initiation of the board with the pieces in the
+     * correct position.
+     * @throws Exception
+     */
+    public void testInitBoard() throws Exception {
+        Board checkersBoard = new Board();
+
+        for(int oddCol = 1, evenCol = 0; oddCol < maxLength; oddCol +=2, evenCol +=2) {
+            //The following block tests the proper instantiation of pieces
+            assertNotNull(checkersBoard.getBoardPiece(0, evenCol));
+            assertNotNull(checkersBoard.getBoardPiece(1, oddCol));
+            assertNotNull(checkersBoard.getBoardPiece(2, evenCol));
+
+            assertNotNull(checkersBoard.getBoardPiece(5, oddCol));
+            assertNotNull(checkersBoard.getBoardPiece(6, evenCol));
+            assertNotNull(checkersBoard.getBoardPiece(7, oddCol));
+
+            //The following tests for the gaps between the checkers pieces instantiation
+            assertNull(checkersBoard.getBoardPiece(0, oddCol));
+            assertNull(checkersBoard.getBoardPiece(1, evenCol));
+            assertNull(checkersBoard.getBoardPiece(2, oddCol));
+
+            assertNull(checkersBoard.getBoardPiece(5, evenCol));
+            assertNull(checkersBoard.getBoardPiece(6, oddCol));
+            assertNull(checkersBoard.getBoardPiece(7, evenCol));
+        }
+
+        for(int row = 3; row < 5; ++row) {
+            for(int col = 0; col < 8; ++col) {
+                assertNull(checkersBoard.getBoardPiece(row, col));
+            }
+        }
+    }
+
     public void testGetBlackTotalPieces() throws Exception {
     	Board checkersBoard = new Board();
     	
