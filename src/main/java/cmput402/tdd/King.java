62afce03cbad0e6a1b12515d161105d2bb68b098
hiufungk <hiufungk@ualberta.ca>
2019-02-22 19:27:24 -0700

    GREEN added implementation for King class getTargetCoord method

--- src/main/java/cmput402/tdd/King.java
+++ src/main/java/cmput402/tdd/King.java
@@ -11,8 +11,36 @@ public class King extends Piece {
 		return super.getColor() + "K";
 	}
 	
-	int[] getTargetCoord(Boolean left, Boolean up, int distance) {
+	int[] getTargetCoord(Boolean left, Boolean up, int distance) throws Exception {
+		if (!(distance == 1 || distance == 2)) {
+			throw new Exception("Distance must be 1 or 2");
+		}
 		
-		return null;
+		int[] target = new int[4];
+		int xDirection = 1;
+		int yDirection = 1;
+		if(left) {
+			yDirection = -1;
+		}
+		if(up) {
+			xDirection = -1;
+		}
+		
+		
+		target[0] = this.getXPosition() + distance * xDirection;
+		target[1] = this.getYPosition() + distance * yDirection;
+		target[2] = distance;
+		
+		if ((left == true) && (up == true)) {
+			target[3] = 3;
+		} else if ((left == true) && (up == false)) {
+			target[3] = 4;
+		} else if ((left == false) && (up == true)) {
+			target[3] = 5;
+		} else if ((left == false) && (up == false)) {
+			target[3] = 6;
+		}
+
+		return target;
 	}
 }
