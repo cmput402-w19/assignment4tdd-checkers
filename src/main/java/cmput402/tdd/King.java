666a30c4960699554f152686225e0f9f590ffbf4
hiufungk <hiufungk@ualberta.ca>
2019-02-22 18:33:23 -0700

    GREEN added implementation for King class constructor

--- src/main/java/cmput402/tdd/King.java
+++ src/main/java/cmput402/tdd/King.java
@@ -1,9 +1,10 @@
 package cmput402.tdd;
 
-public class King {
+public class King extends Piece {
 
-	public King(String color, int xPosition, int yPosition) {
-		
+	public King(String color, int xPosition, int yPosition) throws Exception {
+		super(color, xPosition, yPosition);
+		// TODO Auto-generated constructor stub
 	}
 
 }
