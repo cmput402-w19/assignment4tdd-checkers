52b781d411e4ebc419e196a9e247b7509e8e95fb
Matthew Chung <machung@cs.ualberta.ca>
2019-02-22 18:35:40 -0700

    GREEN initialized constructor to pass test

--- src/main/java/cmput402/tdd/Pawn.java
+++ src/main/java/cmput402/tdd/Pawn.java
@@ -1,9 +1,10 @@
 package cmput402.tdd;
 
-public class Pawn{
+public class Pawn extends Piece{
 	
-	public Pawn(String color, int xPosition, int yPosition) {
-		
+	public Pawn(String color, int xPosition, int yPosition) throws Exception {
+		super(color, xPosition, yPosition);
+		// TODO Auto-generated constructor stub
 	}
 
 }
