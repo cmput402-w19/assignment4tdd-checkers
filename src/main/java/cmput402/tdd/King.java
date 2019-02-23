4e6348a4fdbac75225e47e382fc0cba02f46ca4a
hiufungk <hiufungk@ualberta.ca>
2019-02-22 19:37:15 -0700

    REFACTOR cleaned up if statements for getTargetCoord direction output

--- src/main/java/cmput402/tdd/King.java
+++ src/main/java/cmput402/tdd/King.java
@@ -1,7 +1,7 @@
 package cmput402.tdd;
 
 public class King extends Piece {
-
+	
 	public King(String color, int xPosition, int yPosition) throws Exception {
 		super(color, xPosition, yPosition);
 		// TODO Auto-generated constructor stub
@@ -11,33 +11,39 @@ public class King extends Piece {
 		return super.getColor() + "K";
 	}
 	
+	/**
+     * return the 4 different diagonal coordinate of distance 1 or 2 
+     * output encoding: [targetRow, targetCol, distance, direction] 
+     * where direction:3 is up left, direction:4 is down left, 
+     * direction:5 is up right, direction:6 is down right
+     * @throws Exception 
+     */
 	int[] getTargetCoord(Boolean left, Boolean up, int distance) throws Exception {
 		if (!(distance == 1 || distance == 2)) {
 			throw new Exception("Distance must be 1 or 2");
 		}
 		
 		int[] target = new int[4];
-		int xDirection = 1;
-		int yDirection = 1;
+		int rowDirection = 1;
+		int colDirection = 1;
 		if(left) {
-			yDirection = -1;
+			colDirection = -1;
 		}
 		if(up) {
-			xDirection = -1;
+			rowDirection = -1;
 		}
 		
-		
-		target[0] = this.getXPosition() + distance * xDirection;
-		target[1] = this.getYPosition() + distance * yDirection;
+		target[0] = this.getXPosition() + distance * rowDirection;
+		target[1] = this.getYPosition() + distance * colDirection;
 		target[2] = distance;
 		
-		if ((left == true) && (up == true)) {
+		if (left && up) {
 			target[3] = 3;
-		} else if ((left == true) && (up == false)) {
+		} else if (left && !up) {
 			target[3] = 4;
-		} else if ((left == false) && (up == true)) {
+		} else if (!left && up) {
 			target[3] = 5;
-		} else if ((left == false) && (up == false)) {
+		} else if (!left && !up) {
 			target[3] = 6;
 		}
 
